3c863c9672dfdc56250786e758c2ceeebf9199c7
altintzis <miltiadis.altintzis@students.stamford.edu>
2015-11-23 10:16:07 +0700

    Update Files Week2

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,4 +1,14 @@
 /**
  * 
  */
-document.write("Hello Week2!");
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+var totalPriceEle = document.getElementById("totalPrice");
+
+totalPriceEle.textContent = "$" + total;
