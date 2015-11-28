c44ff23906c8cb4ffe312ee6b0f28dd3573b0875
altintzis <miltiadis.altintzis@students.stamford.edu>
2015-11-28 11:28:29 +0700

    Update file style

--- Week2-2/js/basic-function.js
+++ Week2-2/js/basic-function.js
@@ -3,9 +3,10 @@ var msg = 'Sign up to receive our newsletter for 10% off!';
 
 // Create a function to update the content of the element 
 //whose id attribute has a value of message
-function updateMessage() {
-  var el = document.getElementById('message');
-  el.textContent = msg;
+function updateMessage()
+{
+	var el = document.getElementById('message');
+	el.textContent = msg;
 }
 
 // Call the function
