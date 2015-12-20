e13d3d9f99473edf0d75ebe65e2d612de363ac44
altintzis <miltiadis.altintzis@students.stamford.edu>
2015-12-20 13:51:22 +0700

    Cleaning Code and small improvements

--- Week3/js/game.js
+++ Week3/js/game.js
@@ -1,77 +1,114 @@
 var game =
 {
-	answer :Math.ceil(Math.random()*99), // random this value from 1-99 // (1-99) for lessing only to digits and exclude 0
-	
+	answer : Math.ceil(Math.random() * 99), // random number from 1 to 99
 	tries : 7,
-
 	
 	init : function()
 	{
-		console.log("Random Number = "+this.answer);//for debugging purposes
+		console.log("Random Number = " + this.answer);// for debugging
+		// purposes
 		alert("You have 7 tries to find the correct number!");
 		
 		var self = this;
 		
 		// Number 1
 		var ele = document.getElementById("one");
-		ele.addEventListener("click", function(){self.click(1);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(1);
+		}, false);
+		
 		// Number 2
 		var ele = document.getElementById("two");
-		ele.addEventListener("click", function(){self.click(2);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(2);
+		}, false);
+		
 		// Number 3
 		var ele = document.getElementById("three");
-		ele.addEventListener("click", function(){self.click(3);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(3);
+		}, false);
+		
 		// Number 4
 		var ele = document.getElementById("four");
-		ele.addEventListener("click", function(){self.click(4);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(4);
+		}, false);
+		
 		// Number 5
 		var ele = document.getElementById("five");
-		ele.addEventListener("click", function(){self.click(5);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(5);
+		}, false);
+		
 		// Number 6
 		var ele = document.getElementById("six");
-		ele.addEventListener("click", function(){self.click(6);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(6);
+		}, false);
+		
 		// Number 7
 		var ele = document.getElementById("seven");
-		ele.addEventListener("click", function(){self.click(7);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(7);
+		}, false);
+		
 		// Number 8
 		var ele = document.getElementById("eight");
-		ele.addEventListener("click", function(){self.click(8);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(8);
+		}, false);
+		
 		// Number 9
 		var ele = document.getElementById("nine");
-		ele.addEventListener("click", function(){self.click(9);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(9);
+		}, false);
+		
 		// Number 0
 		var ele = document.getElementById("zero");
-		ele.addEventListener("click", function(){self.click(0);}, false);
-
+		ele.addEventListener("click", function()
+		{
+			self.click(0);
+		}, false);
 		
 		// Guess!
 		var ele = document.getElementById("guess");
-		ele.addEventListener("click", function(){self.checkAnswer();}, false);
+		ele.addEventListener("click", function()
+		{
+			self.checkAnswer();
+		}, false);
 		
-		//clear
+		// clear
 		var ele = document.getElementById("clr");
 		ele.addEventListener("click", this.clearScreen, false);
 		
-		//show
+		// show
 		var ele = document.getElementById("show");
-		ele.addEventListener("click", function(){self.showAnswer();}, false);
-
-		//backspace
+		ele.addEventListener("click", function()
+		{
+			self.showAnswer();
+		}, false);
+		
+		// backspace
 		var ele = document.getElementById("bcsp");
 		ele.addEventListener("click", this.backspace, false);
 		
-		//regenerate number
+		// regenerate number
 		var ele = document.getElementById("reg");
-		ele.addEventListener("click", function(){self.regenerateNumber();}, false);
+		ele.addEventListener("click", function()
+		{
+			self.regenerateNumber();
+		}, false);
 		
 	},
 	
@@ -79,16 +116,17 @@ var game =
 	{
 		var text = document.getElementById("userValue");
 		
-		console.log(text.value.length-1);//for debugging purposes
+		console.log(text.value.length - 1);// for debugging purposes
 		
-		text.value = text.value.substring(0,(text.value.length-1));
+		text.value = text.value.substring(0, (text.value.length - 1));
 	},
 	
 	regenerateNumber : function()
 	{
-		this.answer = Math.ceil(Math.random()*99);
-		console.log("New Random Number = " + this.answer);//for debugging purposes
-		this.tries=7;
+		this.restart();
+		
+		console.log("New Random Number = " + this.answer);// for debugging
+		// purposes
 	},
 	
 	clearScreen : function()
@@ -104,95 +142,94 @@ var game =
 	
 	click : function(value)
 	{
-		console.log(value + " clicked.");
-
+		console.log(value + " clicked.");// for debugging purposes
+		
 		var userValueEle = document.getElementById("userValue");
 		
-
 		if (value == "0" && userValueEle.value == "")
 		{
-			//if first number entering is 0 the program doing nothing
+			// if first number entering is 0 the program is doing nothing
 		}
 		else
 		{
-
+			
 			if (userValueEle.value.length > 1)
 			{
-				//if more than 2 numbers entering the program alerting the user
+				// if more than 2 numbers entering the program alerting the user
 				alert("No more than two numbers is allowed!");
 			}
 			else
 			{
 				userValueEle.value += value;
-
+				
 			}
 		}
 	},
 	
-
 	checkAnswer : function()
 	{
 		var userValueEle = document.getElementById("userValue");
-
+		
 		if (userValueEle.value == this.answer)
 		{
-
+			
 			alert("Congratulations! Correct answer");
-			this.answer = Math.ceil(Math.random()*99);
-			userValueEle.value="";
-			this.tries=7;
-
-			console.log("New Random Number = " + this.answer);//for debugging purposes
+			userValueEle.value = "";
+			this.restart();
+			
+			console.log("New Random Number = " + this.answer);// for debugging
+			// purposes
+		}
+		else if (userValueEle.value > this.answer)
+		{
+			this.tries--;
+			if (this.tries == 0)
+			{
+				alert("Sorry but you did not find the answer, the correct number was " + this.answer);
+				userValueEle.value = "";
+				this.restart();
+			}
+			else if (this.tries == 1)
+			{
+				alert("Guessing number is more than the correct number, this is your last try!!!");
+				userValueEle.value = "";
+			}
+			else
+			{
+				alert("Guessing number is more than the correct number, " + this.tries + " tries has left");
+				userValueEle.value = "";
+			}
+		}
+		else if (userValueEle.value == "")
+		{
+			alert("Please put a number");
 		}
 		else
-			if (userValueEle.value > this.answer)
+		{
+			this.tries--;
+			if (this.tries == 0)
 			{
-				this.tries--;
-				if (this.tries == 0)
-				{
-					alert("Sorry but you did not find the answer, the correct number was " + this.answer);
-					userValueEle.value = "";
-					this.tries=7;
-					this.answer = Math.ceil(Math.random()*99);
-				}
-				else if (this.tries == 1)
-				{
-					alert("Guessing number is more than the correct number, this is your last try!!!");
-					userValueEle.value = "";
-				}
-				else
-				{
-					alert("Guessing number is more than the correct number, "
-							+ this.tries + " tries has left");
-					userValueEle.value = "";
-				}
+				alert("Sorry but you did not find the answer, the correct number was " + this.answer);
+				userValueEle.value = "";
+				this.restart();
 			}
-			else if(userValueEle.value == "")
+			else if (this.tries == 1)
 			{
-				alert("Please put a number");
+				alert("Guessing number is less than the correct number, this is your last try!!!");
+				userValueEle.value = "";
 			}
-			else 
+			else
 			{
-				this.tries--;
-				if (this.tries == 0)
-				{
-					alert("Sorry but you did not find the answer, the correct number was " + this.answer);
-					userValueEle.value = "";
-					this.tries=7;
-					this.answer = Math.ceil(Math.random()*99);
-				}				
-				else if (this.tries == 1)
-				{
-					alert("Guessing number is less than the correct number, this is your last try!!!");
-					userValueEle.value = "";
-				}
-				else
-				{
-					alert("Guessing number is less than the correct number, "
-								+ this.tries + " tries has left ");
-					userValueEle.value = "";
-				}
+				alert("Guessing number is less than the correct number, " + this.tries + " tries has left ");
+				userValueEle.value = "";
 			}
+		}
+	},
+	
+	restart : function()
+	{
+		this.tries = 7;
+		this.answer = Math.ceil(Math.random() * 99);
 	}
 };
 
--- Week5/js/app.js
+++ Week5/js/app.js
@@ -7,7 +7,7 @@ $(function()
 			app.init_effect();
 			app.init_event();
 		},
-
+		
 		init_effect : function()
 		{
 			$("#head_tagline").height(200);
@@ -18,12 +18,13 @@ $(function()
 			
 			$(".error").hide();
 		},
-
+		
 		init_event : function()
 		{
-			$("#head_tagline>a").on("click", function(){
-
-				if($("#head_tagline>a").text()=="Learn More")
+			$("#head_tagline>a").on("click", function()
+			{
+				
+				if ($("#head_tagline>a").text() == "Learn More")
 				{
 					$("#head_tagline").height(300);
 					$("#head_tagline>p:last").fadeIn(700);
@@ -39,15 +40,13 @@ $(function()
 				}
 			});
 			
-
-			
-			
-			$("#form-submit").on("click", function(){
+			$("#form-submit").on("click", function()
+			{
 				
 				var phone = $("#form-number").val();
 				var myText = $("#form-message").val();
-
-				if($.isNumeric(phone))
+				
+				if ($.isNumeric(phone))
 				{
 					$(".error:eq(1)").hide();
 				}
@@ -56,7 +55,7 @@ $(function()
 					$(".error:eq(1)").show();
 				}
 				
-				if(myText.toLowerCase().indexOf("badwords")>=0)
+				if (myText.toLowerCase().indexOf("badwords") >= 0)
 				{
 					$(".error:last").text("Invalid entry: your message may contain bad words");
 					$(".error:last").show();
@@ -66,7 +65,7 @@ $(function()
 					$(".error:last").hide();
 				}
 				
-				if($("#form-name").val()=="")
+				if ($("#form-name").val() == "")
 				{
 					$(".error:first").text("Invalid entry: write a fucking name");
 					$(".error:first").show();
@@ -76,32 +75,30 @@ $(function()
 					$(".error:first").hide();
 				}
 				
-				if($("#form-message").val()=="")
+				if ($("#form-message").val() == "")
 				{
 					$(".error:last").text("Invalid entry: write a fucking message");
 					$(".error:last").show();
 				}
 				
-				
-				if($.isNumeric(phone) && (myText.toLowerCase().indexOf("badwords") < 0) && ($("#form-message").val()!="") && ($("#form-name").val()!=""))
+				if ($.isNumeric(phone) && (myText.toLowerCase().indexOf("badwords") < 0) && ($("#form-message").val() != "") && ($("#form-name").val() != ""))
 				{
 					swal("Message Sent!", "Click ok to continue!", "success");
-
-					 $("#form-name").val("");
-					 $("#form-number").val("");
-					 $("#form-message").val("");
+					
+					$("#form-name").val("");
+					$("#form-number").val("");
+					$("#form-message").val("");
 				}
 			});
-
-						
+			
 			$(".image-flip").flip(
 			{
 				axis : 'y',
 				trigger : 'hover'
 			});
-			
 		}
-
+	
 	};
+	
 	app.init();
 });
