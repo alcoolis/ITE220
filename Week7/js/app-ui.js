973aa6c40ddcf9f40a63f156502a7163535a0222
altintzis <miltiadis.altintzis@students.stamford.edu>
2016-01-08 23:34:01 +0700

    Update copyright

--- Week7/js/app-ui.js
+++ Week7/js/app-ui.js
@@ -4,28 +4,36 @@
 
 function processForData(ajaxResultFor)
 {
-	var date=[ajaxResultFor.list[8].dt_txt, ajaxResultFor.list[16].dt_txt, ajaxResultFor.list[24].dt_txt];
-	var temp=[ajaxResultFor.list[8].main.temp_min, ajaxResultFor.list[16].main.temp_min, ajaxResultFor.list[24].main.temp_min];
+	var index=findTheCorrectDate(ajaxResultFor.list);
+	
+	var date=[ajaxResultFor.list[index].dt_txt, ajaxResultFor.list[index+8].dt_txt, ajaxResultFor.list[index+16].dt_txt];
+	var temp=[ajaxResultFor.list[index].main.temp_min, ajaxResultFor.list[index+8].main.temp_min, ajaxResultFor.list[index+16].main.temp_min];
 	
 	for(var i=0; i<date.length; i++)
 	{
 		date[i]=date[i].substring(0, date[i].indexOf("12:00:00"));
-		$("#cont"+(i+1)+" :nth-child(1)").text(date[i]);
-		$("#cont"+(i+1)+" span:nth-child(1)").text("Temp.:"+temp[i]);
 		
+		$("#cont"+(i+1)+" :nth-child(1)").text(europeanDate(date[i]));
+		$("#cont"+(i+1)+" span:nth-child(1)").text("Temp.:"+temp[i]);
+
+		$("#cont"+(i+1)+"Image").attr("src", "http://openweathermap.org/img/w/" + (ajaxResultFor.list[index].weather[0].icon).substring(0,2)+"d" + ".png");
+		
+		index+=8;
 	}
+}
+
+function europeanDate(date)
+{
+	var year=date.substring(0,4);
+	var month=date.substring(5,7);
+	date=date.substring(8,10);
 	
-	
-	$("#cont1Image").attr("src", "http://openweathermap.org/img/w/" + ajaxResultFor.list[9].weather[0].icon + ".png");
-	$("#cont2Image").attr("src", "http://openweathermap.org/img/w/" + ajaxResultFor.list[17].weather[0].icon + ".png");
-	$("#cont3Image").attr("src", "http://openweathermap.org/img/w/" + ajaxResultFor.list[25].weather[0].icon + ".png");
-	
+	return date + "/" + month + "/" + year;
 }
 
 function processCurData(ajaxResultCur)
 {
 	$("#weatherImage").attr("src", "http://openweathermap.org/img/w/" + ajaxResultCur.weather[0].icon + ".png");
-	// $("#msg").html(ajaxResult.main.temp);
 	
 	$("#container h1").text("Weather Conditions for " + ajaxResultCur.name);
 	
@@ -41,34 +49,25 @@ function processCurData(ajaxResultCur)
 	$("#weatherText p:nth-child(9) span:nth-child(1)").text(formatDate(ajaxResultCur.sys.sunrise, ajaxResultCur.name));
 	$("#weatherText p:nth-child(9) span:nth-child(2)").text(formatDate(ajaxResultCur.sys.sunset, ajaxResultCur.name));
 	
-	loop();
+	//animateWeather();
 	
 }
 
-function loop()
+function animateWeather()
 {
-	$('#weatherImage').css(
-	{
-		right : 50,
-		top : 0
-	});
 	$('#weatherImage').animate(
 	{
 		right : '+=0.5%',
 		top : '+=0.5%',
 	}, 1000, 'swing', false);
-	$('#weatherImage').css(
-	{
-		right : 50,
-		top : 0
-	});
+	
 	$('#weatherImage').animate(
 	{
 		right : '-=0.5%',
 		top : '-=0.5%',
 	}, 1000, 'swing', function()
 	{
-		loop();
+		animateWeather();
 	});
 }
 
@@ -100,9 +99,33 @@ function formatDate(epochTime, cityName)
 			hour += 4;
 			break;
 	}
-	;
 	
 	return pad(hour, 2) + ":" + pad(min, 2) + ":" + pad(sec, 2);
+}
+
+function findTheCorrectDate(ajaxResultList)
+{
+	for (var i = 0; i < ajaxResultList.length; i++)
+	{
+		var string=ajaxResultList[i].dt_txt;
+		
+		var index = string.indexOf("00:00:00");
+		
+		if(index>0)
+		{
+			for (var j = i; j < ajaxResultList.length; j++)
+			{
+				var string=ajaxResultList[j].dt_txt;
+				
+				index = string.indexOf("12:00:00");
+				
+				if(index>0)
+				{
+					return j;
+				}
+			}
+		}
+	}
 }
 
 function pad(num, size)
